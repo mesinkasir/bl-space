1caa0ecef8a76115c731a8a8146ce3fdc5567cd2
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2023-03-15 07:33:50 +0700

    Update home.php

--- php/home.php
+++ php/home.php
@@ -2,6 +2,26 @@
 <div class="row">
 <?php include(THEME_DIR_PHP.'404.php'); ?>
 <div class="row">
+  <?php if (pluginActivated('pluginSearch')): ?>
+<div class="form-inline d-block">
+<input id="search-input" class="form-control mr-sm-2" type="search" placeholder="<?php $language->p('Search') ?>" aria-label="Search">
+<button class="btn btn-outline-dark my-2 my-sm-0" type="button" onClick="searchNow()"><?php $language->p('Search') ?></button>
+<script>
+function searchNow() {
+var searchURL = "<?php echo Theme::siteUrl(); ?>search/";
+window.open(searchURL + document.getElementById("search-input").value, "_self");
+}
+document.getElementById("search-input").onkeypress = function(e) {
+if (!e) e = window.event;
+var keyCode = e.keyCode || e.which;
+if (keyCode == '13') {
+searchNow();
+return false;
+}
+}
+</script>
+</div>
+<?php endif ?>
 <?php foreach ($content as $page): ?>
 <div class="col-md-4 p-3">
 <?php Theme::plugins('pageBegin'); ?>
